142955a06595c8fd80e7c7ff30cc518dc35d95bb
iRender1 <86774748+iRender1@users.noreply.github.com>
2024-05-10 21:50:07 +0300

    Add files via upload

---
+++ dialoguemenu/config.lua
@@ -0,0 +1,25 @@
+Config = {}
+
+Config.npcs = {
+    {
+        name = "ابو سلمان",
+        text = "مرحبًا، تريد بدء دوامك او الانتهاء منه؟",
+        job = "موظف برقر شوت",
+        ped = "csb_burgerdrug",
+        coords = vector4(-1191.25, -900.46, 12.98, 311.28),
+        options = {
+            {
+                label = "تسجيل دخول/خروج",
+                event = "",
+                type = "",
+                args = {'1'} -- Arguments for the command
+            },
+            {
+                label = "هونت مالي خلق اداوم",
+                event = "",
+                type = "",
+                args = {'2'} -- Arguments for the command
+            },
+        }
+    },
+}
---
+++ dialoguemenu/fxmanifest.lua
@@ -0,0 +1,17 @@
+fx_version "adamant"
+game "gta5"
+
+ui_page "ui/index.html"
+files {
+    "ui/**/**",
+}
+
+shared_scripts {
+	'config.lua'
+}
+
+client_scripts {
+	"client.lua"
+}
+
+
